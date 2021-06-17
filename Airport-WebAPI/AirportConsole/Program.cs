df4742c8b2cbec1003168d1515d86bbd49d8d240
David Brais <2095059@cshawi.ca>
2021-06-17 13:49:38 -0400

    done!

--- Airport-WebAPI/AirportConsole/Program.cs
+++ Airport-WebAPI/AirportConsole/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using AirShawi;
+
 namespace AirportConsole
 {
     class Program
     {
         static Airports airports;
+        
+        
         static System.Net.Http.HttpClient HttpAirShawi = new System.Net.Http.HttpClient();
         static void Main(string[] args)
         {
             HttpAirShawi.BaseAddress = new Uri("http://localhost/Airport-WebAPI");
             airports = new Airports(HttpAirShawi);
+            
             int choice;
             do
             {
@@ -28,7 +32,7 @@ namespace AirportConsole
                         ListAirport();
                         break;
                     case 2:
-                        ModifyAirport();
+                        ModifyMenu();
                         break;
 
 
@@ -42,17 +46,63 @@ namespace AirportConsole
 
         static void ListAirport()
         {
-            foreach(Airport airport in airports.GetAll().Result)
+            Console.Clear();
+            Console.WriteLine("Voici la liste des aéroports:\n\n  {0,-5} {1,-14} {2, 3}", "Code", "Ville", "Nom\n");
+            foreach (Airport airport in airports.GetAll().Result)
             {
-                Console.WriteLine($"{airport.IataCode} - {airport.Name}");
+                Console.WriteLine("  {0,-5} {1,-14} {2, 6}", airport.IataCode, airport.City, airport.Name);
+                
             }
             Console.ReadKey();
         }
 
-        static void ModifyAirport()
+        static bool AirportExists(string key)
         {
-            Console.WriteLine("modify Airport TODO");
+            foreach (Airport a in airports.GetAll().Result)
+            {
+                if (a.IataCode == key.ToUpper()) return true;
+            }return false;
+        }
+
+        static void ModifyMenu()
+        {
+            Console.Clear();
+            Console.Write("inscrivez le code de l'aeroport que vous désirez modifier : ");
+            string key = Console.ReadLine();
+            
+            if (AirportExists(key))
+            {
+                Airport airportToModify = airports.GetById(key).Result;
+                Console.WriteLine($"Code IATA : {airportToModify.IataCode}\n" +
+                    $"Ville : {airportToModify.City}\n" +
+                    $"Nom : {airportToModify.Name}\n");
+
+                if (ModifyAirport(airportToModify)) Console.WriteLine("Les modifications ont été apportées avec succès.");
+                else Console.WriteLine("Une erreure s'est produite.");
+            }
+            else
+            {
+                Console.WriteLine("Ce code n'existe pas. Veuillez reessayer");
+                Console.ReadKey();
+                ModifyMenu();
+            }
+
             Console.ReadKey();
+        }
+
+        static bool ModifyAirport(Airport airportToModify)
+        {
+            string newName, newCity;
+            Console.WriteLine();
+            Console.Write("Nouvelle ville : ");
+            newCity = Console.ReadLine();
+            Console.Write("Nouveau nom : ");
+            newName = Console.ReadLine();
+            airportToModify.Name = newName;
+            airportToModify.City = newCity;
+            airports.Put(airportToModify.IataCode, airportToModify);
+            return airports.GetById(airportToModify.IataCode).Result.Name == newName ?  true :  false;
+            
         }
 
         static void printMenu()
